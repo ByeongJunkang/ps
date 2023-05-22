17e4c520ccf2da1653409b6aa04b96a9eadc5ab6
강병준 <freudbjk@gmail.com>
2023-05-22 20:57:58 +0900

    백준 1051.py

---
+++ 백준/구현/백준 1051.py
@@ -0,0 +1,29 @@
+
+
+
+def find_sqr(s):
+    for i in range(N-s+1):
+        for j in range(M-s+1):
+            if arr[i][j] == arr[i][j+s-1] == arr[i+s-1][j] == arr[i+s-1][j+s-1]:
+                return True
+    
+
+    return False
+
+
+answer = 0
+
+N, M = map(int,input().split())
+
+arr = [ list(map(int,input())) for _ in range(N)]
+
+size = min(N,M)
+for k in range(size,0,-1):
+    if find_sqr(k):
+        answer = k ** 2
+        break
+
+
+print(answer)
+
+
