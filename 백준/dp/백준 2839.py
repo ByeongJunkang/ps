f3992a1aa33aebdc75385ddec03e48a844208b72
강병준 <freudbjk@gmail.com>
2023-06-15 16:51:27 +0900

    2839 수정

--- 백준/dp/백준 2839.py
+++ 백준/dp/백준 2839.py
@@ -1,10 +1,9 @@
 N = int(input())
-dp = [5001] * (5001)
+dp = [5001] * (N+5)
 dp[3] = 1
 dp[5] = 1
 for i in range(6,N+1):
     dp[i]= min(dp[i-3],dp[i-5])+1
-if dp[N] == 5001:
-    print(-1)
-else:
-    print(dp[N])
+print(dp[N] if dp[N] < 5001 else -1)
+
+    
