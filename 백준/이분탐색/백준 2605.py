0fba01a5c387e7a27868ba7e5604638fee868453
강병준 <freudbjk@gmail.com>
2023-06-21 17:54:24 +0900

    2605

--- 백준/이분탐색/백준 2605.py
+++ 백준/이분탐색/백준 2605.py
@@ -1,1 +1,14 @@
 N, M = map(int,input().split())
+trees = list(map(int,input().split()))
+left , right = 0, max(trees)
+while left<=right:
+    middle = (left+right) //2
+    cut = 0
+    for i in trees:
+        if i  > middle:
+            cut+= i-middle
+    if cut >= M:
+        left = middle +1
+    else:
+        right = middle -1
+print(right)
