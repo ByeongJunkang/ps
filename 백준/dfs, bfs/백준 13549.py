6809dc6035943001a39cf18a47b614db061e3de9
강병준 <freudbjk@gmail.com>
2023-07-11 23:22:54 +0900

    13549

--- 백준/dfs, bfs/백준 13549.py
+++ 백준/dfs, bfs/백준 13549.py
@@ -11,23 +11,22 @@ def bfs():
     while queue:
         global shortest_path
         cur_num,cur_len = queue.popleft()
-        nx = [-1,1,2* cur_num]
+        nx = [2* cur_num,-1,1]
         
         if cur_num == n:
-            print("hi")
             shortest_path = cur_len
             break
 
         for i in range(3):
-            if i != 2:
+            if i != 0:
                 next_x = cur_num + nx[i]
             else:
                 next_x = cur_num * 2
         
             if next_x >=0 and next_x < 100001:
                 if not visited[next_x]:
-                    if i == 2:
-                        queue.append((next_x,cur_len))
+                    if i == 0:
+                        queue.appendleft((next_x,cur_len))
                     else:
                         queue.append((next_x,cur_len+1))
                     visited[next_x] = True
---
+++ 백준/백준 3758.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+
+#n = 팀개수
+# k = 문제개수
+# t 내팀
+# m = 로그
+for _ in range(T):
+    n,k,t,m = map(int,input().split())
+    
