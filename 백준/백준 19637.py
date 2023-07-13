4828f4bb8a9460c624ecbfb4d257f2e6a735a5a2
강병준 <freudbjk@gmail.com>
2023-07-13 14:40:29 +0900

    19637

--- 백준/백준 19637.py
+++ 백준/백준 19637.py
@@ -1,12 +1,18 @@
 import sys
 input = sys.stdin.readline
 n,m = map(int,input().split())
-standard = {}
+standard = []
 for _ in range(n):
     a,b = input().rstrip().split()
-    standard[a] = int(b)
-
-print(standard)
+    standard.append([a,b])
 for _ in range(m):
     a = int(input())
-    print(a)
+    left = 0
+    right = n-1
+    while left<=right:
+        mid = (left + right) //2
+        if int(standard[mid][1]) < a:
+            left = mid + 1
+        else:
+            right = mid- 1
+    print(standard[right+1][0])
