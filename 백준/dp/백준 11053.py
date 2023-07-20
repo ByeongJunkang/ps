c36c62445e1fcd3d25b7ea248eea3851209e056b
강병준 <freudbjk@gmail.com>
2023-07-20 14:37:08 +0900

    11053 리팩토링

--- 백준/dp/백준 11053.py
+++ 백준/dp/백준 11053.py
@@ -10,10 +10,7 @@ for i in range(1,A):
         if num[i] > num[j]:
             if num[j] >= max_num:
                 max_num = max(dp[j],max_num)
-                max_index = False
-    if max_index:
-        dp[i] = 1
-    else:
+    
         dp[i] = max_num+ 1
 
 print(max(dp))
