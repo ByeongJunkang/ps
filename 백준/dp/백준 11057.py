0ea229c366676e8868ece3b45bdf5fe544472d9d
강병준 <freudbjk@gmail.com>
2023-07-22 16:08:55 +0900

    11057

---
+++ 백준/dp/백준 11057.py
@@ -0,0 +1,10 @@
+N = int(input())
+dp = [[0] * 10 for _ in range(N+1)]
+
+dp[1] = [1] * 10
+
+for i in range(1,N+1):
+    for j in range(10):
+        for k in range(0,j+1):
+            dp[i][j] +=dp[i-1][k]
+print(sum(dp[N]) % 10007)
