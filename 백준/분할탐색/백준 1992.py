f21fb2a534c445ee8e5a4efe67d3f3c79243b0d7
강병준 <freudbjk@gmail.com>
2023-07-29 15:27:04 +0900

    gpt 리팩토링

--- 백준/분할탐색/백준 1992.py
+++ 백준/분할탐색/백준 1992.py
@@ -30,33 +30,60 @@
 # check(0,0,N)
 # print(*result)
 
+# import sys
+# input = sys.stdin.readline
+
+# N = int(input())
+# video = []
+# for _ in range(N):
+#     video.append(list(map(int, input().rstrip())))
+
+# # 
+# def check(x, y, N):
+#     # N이 1보다 작으면 return
+#     if N < 1:
+#         return None
+    
+#     # 특정 범위 내 숫자들이 
+#     isTrue = True
+#     first = video[x][y]
+
+#     for i in range(x, x + N):
+#         for j in range(y, y + N):
+#             if video[i][j] != first:
+#                 isTrue = False
+#                 break
+#         if not isTrue:
+#             break
+    
+#     if isTrue:
+#         return str(first)
+    
+#     else:
+#         half_N = N // 2
+#         return "(" + check(x, y, half_N) + check(x, y + half_N, half_N) + check(x + half_N, y, half_N) + check(x + half_N, y + half_N, half_N) + ")"
+
+# result = check(0, 0, N)
+# print(result)
+
 import sys
-input = sys.stdin.readline
 
-N = int(input())
-video = []
-for _ in range(N):
-    video.append(list(map(int, input().rstrip())))
+def read_input():
+    return sys.stdin.readline().rstrip()
 
-def check(x, y, N):
-    if N < 1:
+def compress_video(x, y, size):
+    if size < 1:
         return None
     
-    isTrue = True
     first = video[x][y]
-    for i in range(x, x + N):
-        for j in range(y, y + N):
-            if video[i][j] != first:
-                isTrue = False
-                break
-        if not isTrue:
-            break
-    
-    if isTrue:
+    if all(video[i][j] == first for i in range(x, x + size) for j in range(y, y + size)):
         return str(first)
     else:
-        half_N = N // 2
-        return "(" + check(x, y, half_N) + check(x, y + half_N, half_N) + check(x + half_N, y, half_N) + check(x + half_N, y + half_N, half_N) + ")"
+        half_size = size // 2
+        return f"({compress_video(x, y, half_size)}{compress_video(x, y + half_size, half_size)}" \
+               f"{compress_video(x + half_size, y, half_size)}{compress_video(x + half_size, y + half_size, half_size)})"
 
-result = check(0, 0, N)
+N = int(read_input())
+video = [list(map(int, read_input())) for _ in range(N)]
+result = compress_video(0, 0, N)
 print(result)
