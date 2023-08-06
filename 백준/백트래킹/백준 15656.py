907b3a3a756c590333eb3341037854d9f9634b41
강병준 <freudbjk@gmail.com>
2023-08-06 15:59:25 +0900

    15656

---
+++ 백준/백트래킹/백준 15656.py
@@ -0,0 +1,12 @@
+N, M = map(int,input().split())
+num_list = sorted(list(map(int,input().split())))
+result = []
+def dfs(i):
+    if len(result) == M:
+        print(*result)
+        return
+    for i in range(N):
+        result.append(num_list[i])
+        dfs(i)
+        result.pop()
+dfs(0)
