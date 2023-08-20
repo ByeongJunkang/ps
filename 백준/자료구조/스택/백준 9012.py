1be953353b2016ce1e586606883309aa15040d28
강병준 <freudbjk@gmail.com>
2023-08-20 15:00:59 +0900

    9012

---
+++ 백준/자료구조/스택/백준 9012.py
@@ -0,0 +1,19 @@
+N = int(input())
+for _ in range(N):
+    symbol = list(input())
+    stack = []
+    for letter in symbol:
+        if letter == '(':
+            stack.append(letter)
+        elif letter == ')' and len(stack) > 0:
+            if stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(letter)
+        
+        else:
+            stack.append(letter)
+    if len(stack) > 0:
+        print("NO")
+    else:
+        print("YES")
