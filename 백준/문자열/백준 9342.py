a738984c1c5af31b62b436f9626f128e68bdae9c
강병준 <freudbjk@gmail.com>
2023-09-07 15:27:13 +0900

    9342

--- 백준/문자열/백준 9342.py
+++ 백준/문자열/백준 9342.py
@@ -2,20 +2,51 @@ t = int(input())
 
 for _ in range(t):
     a = list(input())
-
     if ord(a[0]) >= ord('G'):
-        print("GOOD")
+        print("Good")
         continue
-
-    first,second,third = 0
-    for i in range(a):
-        if i == "A" and first == 0:
+    first,second,third = 0,0,0
+    if a[0] == 'A':
+        first =1
+    flag = True
+    for i in range(1,len(a)):
+        if a[i] == "A" and first == 0:
             first = 1
             continue
-        elif i !="A" and first ==0:
-            print("GOOD")
-            continue
-        if first ==1 and second ==0 and third ==0 and a[i] != 'A' and a[i] !="B":
+        elif a[i] !="A" and first ==0:
+            flag = False
+            break
+        if first ==1 and second ==0 and third ==0: 
+            if a[i] == 'A':
+                continue
+            elif a[i] =="F":
+                second =1
+                continue
+            else:
+                flag = False
+                break
+        
+        elif first == 1 and second == 1 and third == 0:
+            if a[i] == 'A':
+                continue
+            elif a[i] =="F":
+                continue
+            elif a[i] == "C":
+                third = 1
+                continue
+            else:
+                flag = False
+                break
 
+        elif first ==1 and second == 1 and third == 1:
+            if ord(a[i]) >= ord("G"):
+                flag = False
+                break
+        
+
+    if flag  == True:
+        print("Infected!")
+    else:
+        print("Good")
 
         
