72a9d3d966d78eaa2daa76c0c1408cc3953cb6ca
강병준 <freudbjk@gmail.com>
2023-09-11 15:17:25 +0900

    1059

---
+++ 백준/구현/백준 1913.py
@@ -0,0 +1,13 @@
+# n = int(input())
+# dx = [0,1,2,3]
+# ans = [[0]* n for _ in range(n)]
+# for i in range(1,26):
+#     if dx[loc] == 0:
+#         while ans
+#     elif dx[loc] == 1:
+
+
+#     elif dx[loc] == 2:
+
+
+#     elif dx[loc] == 3:
