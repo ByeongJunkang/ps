a3c2e998152703bf4e785fbbcaea522dd375bb63
강병준 <freudbjk@gmail.com>
2023-09-19 20:25:36 +0900

    13413 수정

--- 백준/백준 13413.py
+++ 백준/백준 13413.py
@@ -6,11 +6,15 @@ for _ in range(t):
     n = int(input())
     origin = list(input())
     goal = list(input())
-    W_num = abs(origin.count("W") - goal.count("W"))
+    w_cnt = 0
+    b_cnt = 0
     for i in range(n):
         if origin[i] != goal[i]:
-            cnt+=1
-    if cnt > W_num:
-        W_num += int((cnt -W_num)/2)
-    print(W_num)
+            if origin[i] == "W":
+                w_cnt +=1
+            else:
+                b_cnt +=1
+
+   
+    print(max(b_cnt,w_cnt))
                 
